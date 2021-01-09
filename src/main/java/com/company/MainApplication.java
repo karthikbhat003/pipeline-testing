01330891992e1bef8c6eb07d76bb6f30f926e275
karthikbhat003 <karthik.bhat@mhealth.tech>
2021-01-10 00:49:01 +0530

    add resource config to read yml file

--- src/main/java/com/company/MainApplication.java
+++ src/main/java/com/company/MainApplication.java
@@ -3,11 +3,19 @@ package com.company;
 import com.company.configuration.TestConfiguration;
 import com.company.resource.TestResource;
 import io.dropwizard.Application;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class MainApplication extends Application<TestConfiguration> {
     public static void main(String[] args) throws Exception {
         new MainApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<TestConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new ResourceConfigurationSourceProvider());
     }
 
     public void run(TestConfiguration testConfiguration, Environment environment) throws Exception {
