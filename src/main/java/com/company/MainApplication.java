9fe07d916d69fb07bfbcd4da9f716fe02228eb08
karthikbhat003 <karthik.bhat@mhealth.tech>
2021-01-10 08:42:23 +0530

    Add config in yml

--- src/main/java/com/company/MainApplication.java
+++ src/main/java/com/company/MainApplication.java
@@ -3,10 +3,14 @@ package com.company;
 import com.company.configuration.TestConfiguration;
 import com.company.resource.TestResource;
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainApplication extends Application<TestConfiguration> {
     public static void main(String[] args) throws Exception {
         new MainApplication().run(args);
@@ -17,11 +21,16 @@ public class MainApplication extends Application<TestConfiguration> {
         /**
          * This is required for reading yml from config.
          */
+//        bootstrap.setConfigurationSourceProvider(new ResourceConfigurationSourceProvider());
+        /**
+         * This is required to read the environment variables
+         */
         bootstrap.setConfigurationSourceProvider(
-                new ResourceConfigurationSourceProvider());
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(), new EnvironmentVariableSubstitutor(false)));
     }
 
     public void run(TestConfiguration testConfiguration, Environment environment) {
+        log.info("Configuration is available in running environment {}", testConfiguration.toString());
         final TestResource testResource = new TestResource();
         environment.jersey().register(testResource);
     }
--- src/main/java/com/company/configuration/TestConfiguration.java
+++ src/main/java/com/company/configuration/TestConfiguration.java
@@ -1,7 +1,19 @@
 package com.company.configuration;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.dropwizard.Configuration;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties (ignoreUnknown = true)
+@JsonInclude (value = JsonInclude.Include.NON_NULL)
+@ToString
 public class TestConfiguration extends Configuration {
-
+    private String template;
 }
